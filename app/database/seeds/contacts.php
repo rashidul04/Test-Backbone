af7c8014178a11db99fecce1af11436acfdb20ed
Rashidul <rashidul.ese@gmail.com>
2012-12-05 08:32:47 +0600

    added necessary BB files 2

---
+++ app/database/seeds/contacts.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+	array(
+		'first_name' => 'John',
+		'last_name'  => 'Doe',
+		'email_address' => 'john@example.com',
+		'description' => 'My best friend.'
+	),
+	array(
+		'first_name' => 'Jane',
+		'last_name'  => 'Doe',
+		'email_address' => 'jane@example.com',
+		'description' => 'My best friend\'s wife.'
+	),
+	array(
+		'first_name' => 'Rashidul',
+		'last_name'  => 'Islam',
+		'email_address' => 'rashidul.ese@gmail.com',
+		'description' => 'Not sure why I have myself as a contact, but okay.'
+	)
+);
